f1d1e5e08c9e2bd01b73e5f6fc3bdc3cb3269fd9
HYEONGJUN-JANG <jun1057@hanyang.ac.kr>
2022-04-25 15:21:10 +0000

    HJ results 용

---
+++ autotest.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+python main.py --max_body_len=100 --reg_term=1.0
+# python main.py --max_body_len=100 --reg_term=1.0
+# python main.py --max_body_len=100 --reg_term=1.0
+python main.py --max_body_len=100 --reg_term=0.5
+# python main.py --max_body_len=100 --reg_term=0.5
+# python main.py --max_body_len=100 --reg_term=0.5
+python main.py --max_body_len=100 --reg_term=0.2
+# python main.py --max_body_len=100 --reg_term=0.2
+# python main.py --max_body_len=100 --reg_term=0.2
+python main.py --max_body_len=100 --reg_term=0.1
+# python main.py --max_body_len=100 --reg_term=0.1
+# python main.py --max_body_len=100 --reg_term=0.1
+python main.py --max_body_len=100 --reg_term=0.01
+# python main.py --max_body_len=100 --reg_term=0.01
+# python main.py --max_body_len=100 --reg_term=0.01
+python main.py --max_body_len=100 --reg_term=0
+# python main.py --max_body_len=100 --reg_term=0
+# python main.py --max_body_len=100 --reg_term=0
