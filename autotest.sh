95ebd5b627301d426bdc1b62a2964b613aee4adf
HYEONGJUN-JANG <jun1057@hanyang.ac.kr>
2022-04-28 07:06:23 +0000

    autotest.sh

--- autotest.sh
+++ autotest.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-<<<<<<< HEAD
 python main.py --max_body_len=100 --batch_size=32 --reg_term=1
 python main.py --max_body_len=100 --batch_size=32 --reg_term=0.5
 python main.py --max_body_len=100 --batch_size=32 --reg_term=0.2
@@ -8,4 +7,3 @@ python main.py --max_body_len=100 --batch_size=32 --reg_term=0.01
 python main.py --max_body_len=100 --batch_size=32 --reg_term=0.02
 python main.py --max_body_len=100 --batch_size=32 --reg_term=0.05
 python main.py --max_body_len=100 --batch_size=32 --reg_term=0
->>>>>>> 258207b36d5551f1cdccfdece0eac0d3c3959430
