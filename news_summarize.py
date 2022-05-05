8072d7150f37b8880dcebcc092a1436d8a401489
taeho <hirooms2@hanyang.ac.kr>
2022-05-05 13:51:27 +0900

    summrizer

--- news_summarize.py
+++ news_summarize.py
@@ -15,15 +15,12 @@ summarizer = Summarizer()
 for i, path in enumerate([train_path, dev_path, test_path]):
     text_path = os.path.join(path, 'news_with_body.tsv')
     w_text_path = os.path.join(path, 'news_with_summarized.tsv')
-
     with open(text_path, 'r', encoding='utf-8') as rf:
-        with open(w_text_path, 'w', encoding='utf-8') as wf:
-            for line in tqdm(rf):
+        with open(w_text_path, 'a', encoding='utf-8') as wf:
+            for idx, line in enumerate(tqdm(rf)):
                 splited = line.strip('\n')
                 splited2 = splited.split('\t')
                 doc_id, category, subcategory, title, abstract, _, title_entities, _, body = splited2
-
                 summarized = summarizer(body, num_sentences=3)
-
                 s = '%s\t%s\n' % (splited, summarized)
                 wf.write(s)
--- preprocess.py
+++ preprocess.py
@@ -76,12 +76,12 @@ def read_news(data_path, args, tokenizer):
     test_path = os.path.join(data_path, 'test')
 
     for i, path in enumerate([train_path, dev_path, test_path]):
-        text_path = os.path.join(path, 'news_with_body.tsv')
+        text_path = os.path.join(path, 'news_with_summarized.tsv')
         with open(text_path, 'r', encoding='utf-8') as f:
 
             for line in tqdm(f):
                 splited = line.strip('\n').split('\t')
-                doc_id, category, subcategory, title, abstract, _, title_entities, _, body = splited
+                doc_id, category, subcategory, title, abstract, _, title_entities, _, body, sbody = splited
                 if doc_id in news_index:
                     continue
                 news_index[doc_id] = index
